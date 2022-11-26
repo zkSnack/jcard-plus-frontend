dfb20dff15983ea60a078f61eda90a5a02468a59
Anshul Singhal <sganshuliiith@gmail.com>
2022-11-26 16:55:57 -0500

    [QR code] Embeds QR code scanner to scan auth request

--- src/components/request-components/addProofDialog.js
+++ src/components/request-components/addProofDialog.js
@@ -1,36 +1,42 @@
-import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Button } from '@mui/material'
+import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from '@mui/material'
+import QrReader from 'react-qr-scanner'
 
 function  AddProofDialog({open, handleClose, handleAdd}) {
-  let url = "";
 
-  const handleUrlChange = (event) => {
-    url = event.target.value;
+  const handleScan = (data) => {
+      if (data != null ) {
+        data = JSON.parse(data.text);
+        if (data.hasOwnProperty('url')) {
+            handleAdd(data)
+        }
+    }
   }
 
-  return (
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Add Proof Request</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Please enter the URL of the proof request you would like to add.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="proofRequestURL"
-            label="Proof Request URL"
-            type="url"
-            fullWidth
-            variant="standard"
-            onChange={handleUrlChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={() => handleAdd(url)}>Add</Button>
-        </DialogActions>
-      </Dialog>
-  )
+  const previewStyle = {
+    height: 240,
+    width: 320,
+  }
+
+  const handleError = (data) => {
+      console.log(data);
+  }
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add Proof Request</DialogTitle>
+            <DialogContent>
+                <QrReader
+                    delay={100}
+                    style={previewStyle}
+                    onError={handleError}
+                    onScan={handleScan}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
 
 export default AddProofDialog
