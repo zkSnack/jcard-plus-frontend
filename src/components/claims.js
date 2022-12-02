ac716c33dac87c30edc84a0e4cd0d280a7243556
Pratik Kayastha <pratikkayastha98@gmail.com>
2022-12-02 02:52:58 -0500

    Refactor code to allow users to add claims for UI

--- src/components/claims.js
+++ src/components/claims.js
@@ -1,28 +1,60 @@
-import { RotatingLines } from 'react-loader-spinner';
-import { Typography, Paper } from "@mui/material";
+import { Typography, Paper, CircularProgress, Backdrop } from "@mui/material";
 import { useState, useEffect } from "react";
 import { Grid } from "@mui/material";
 
 import AddClaimDialog from "./request-components/addClaimDialog";
 import Button from '@mui/material/Button';
 import Claim from "./claim";
-import React from "react";
-import axios from 'axios';
 
 function Claims() {
     const [claims, setClaims] = useState([])
-    const [open, setOpen] = React.useState(false);
-    const [selectedValue, setSelectedValue] = React.useState();
-    const [reload, setReload] = React.useState(false);
-    const [loading, setLoading] = React.useState(true);
+    const [open, setOpen] = useState(false);
+    const [reload, setReload] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const getClaimsByIssuer = async (issuerURL) => {
+        if(issuerURL) {
+            setLoading(true);
+            let protocol = 'https://';
+            // Add protocol if not present
+            if (issuerURL.startsWith('http') || issuerURL.startsWith('https')) {
+                protocol = '';
+            } else if (issuerURL.includes('localhost')) {
+                protocol = 'http://';
+            }
+            const body = {
+                issuerURL: protocol + issuerURL
+            };
+            try {
+                // Use this for testing with a local file
+                // await fetch('http://localhost:8000/fetchClaimsResponse.json', {
+                //     method: 'GET',
+                //     mode: 'cors',
+                // });
+                await fetch('http://localhost:8080/api/v1/fetchClaimsByIssuer', {
+                    method: 'POST',
+                    mode: 'cors',
+                    body: JSON.stringify(body),
+                });
+                setReload(reload => !reload);
+            } catch (error) {
+                console.log("Error: ", error);
+            }
+            setLoading(false);
+        }
+    }
   
     const handleClickOpen = () => {
         setOpen(true);
     };
-  
-    const handleClose = (value) => {
+
+    const handleDialogClose = () => {
         setOpen(false);
-        setSelectedValue(value);
+    }
+  
+    const handleDialogSelection = (value) => {
+        setOpen(false);
+        getClaimsByIssuer(value);
     };
 
     useEffect(() => {
@@ -32,57 +64,35 @@ function Claims() {
             //     method: 'GET',
             //     mode: 'cors',
             // });
-            const response = await fetch('http://localhost:8080/api/v1/getClaims', {
-                method: 'GET',
-                mode: 'cors',
-            });
-            const data = await response.json();
-            if (data.hasOwnProperty('claims')) {
-                setClaims(data.claims || []); // if data.claims is null, set it to an empty array
+            try {
+                const response = await fetch('http://localhost:8080/api/v1/getClaims', {
+                    method: 'GET',
+                    mode: 'cors',
+                });
+                const data = await response.json();
+                if (data.hasOwnProperty('claims')) {
+                    setClaims(data.claims || []); // if data.claims is null, set it to an empty array
+                }
+            } catch (error) {
+                console.log("Error: ", error);
             }
         }
         getClaims();
-    }, [reload])
-
-    useEffect(() => {
-        async function getClaimsByIssuer() {
-            let issuerURL = 'http://localhost:8080/api/v1/fetchClaimsByIssuer';
-            await axios.post(issuerURL, {
-                issuerURL: selectedValue
-            })
-            .then((response) => {
-                // view by response.data
-                setReload(!reload);
-            })
-            .catch((error) => {
-                console.log("Error: ", error)
-            })
-            setLoading(false);
-        }
-        getClaimsByIssuer();
-        setLoading(true)
-    }, [selectedValue])
+    }, [reload]);
     
     return (
         <>
             <Button variant="outlined" onClick={handleClickOpen}>ADD CLAIM REQUEST</Button>
             <AddClaimDialog
-                selectedValue={selectedValue}
                 open={open}
-                onClose={handleClose}/>
-            {loading && 
-                <div style={{position:'fixed', top:'50%', left:'50%', transform:'translate(-50%, -50%)', color:'red'}}>
-                    <RotatingLines 
-                        height="80"
-                        width="80"
-                        radius="9"
-                        strokeColor="lightblue"
-                        ariaLabel="loading"
-                        wrapperStyle
-                        wrapperClass
-                    />
-                </div>
-            }
+                onClose={handleDialogClose}
+                onSelect={handleDialogSelection}/>
+            <Backdrop
+                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                open={loading}
+            >
+                <CircularProgress color="inherit" />
+            </Backdrop>
             <Paper elevation={5} sx={{width: "15%", padding: "1rem", textAlign: "center", margin: "1rem"}}>
                 <Typography> Total Claims: {claims.length} </Typography>
             </Paper>
--- src/components/request-components/addClaimDialog.js
+++ src/components/request-components/addClaimDialog.js
@@ -1,61 +1,97 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
+import { useState } from 'react';
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, ListItemButton } from '@mui/material';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField, Button } from '@mui/material'
 import PersonIcon from '@mui/icons-material/Person';
 import AddIcon from '@mui/icons-material/Add';
 import { blue } from '@mui/material/colors';
 
-const issuerURLs = ['localhost:8080', 'localhost:1234'];
+const issuerURLs = ['localhost:8090'];
 
-export interface SimpleDialogProps {
-  open: boolean;
-  selectedValue: string;
-  onClose: (value: string) => void;
-}
-
-function AddClaimDialog(props: SimpleDialogProps) {
-  const { onClose, selectedValue, open } = props;
-
-  const handleClose = () => {
-    onClose(selectedValue);
-  };
+function AddClaimDialog({onClose, open, onSelect}) {
+  const [issuerURL, setIssuerURL] = useState('');
+  const [addIssuerOpen, setAddIssuerOpen] = useState(false);
 
   const handleListItemClick = (value) => {
     if (value == null) {
       value = issuerURLs[0];
     }
-    onClose(value);
+    onSelect(value);
+  };
+
+  // TODO: Check if issuerURL is valid and already exists in issuerURLs or not
+  const addIssuerURL = () => {
+    issuerURLs.push(issuerURL);
+    setIssuerURL('');
+    setAddIssuerOpen(false);
+  };
+
+  const addIssuer = () => {
+    setAddIssuerOpen(true);
+  };
+
+  const handleAddIssuerClose = () => {
+    setAddIssuerOpen(false);
+  };
+
+  const handleUrlChange = (event) => {
+    setIssuerURL(event.target.value);
   };
 
   return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Select claim issuer</DialogTitle>
-      <List sx={{ pt: 0 }}>
-        {issuerURLs.map((issuerURL) => (
-          <ListItem onClick={() => handleListItemClick(issuerURL)} key={issuerURL}>
-            <ListItemAvatar>
-              <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                <PersonIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText primary={issuerURL} />
+    <>
+      <Dialog onClose={() => onClose()} open={open}>
+        <DialogTitle sx={{
+          textAlign: 'center',
+        }}>Select Claim Issuer</DialogTitle>
+        <List sx={{ pt: 0 }}>
+          {issuerURLs.map((issuerURL) => (
+            <ListItem key={issuerURL}>
+              <ListItemButton onClick={() => handleListItemClick(issuerURL)}>
+                <ListItemAvatar>
+                  <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                    <PersonIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={issuerURL} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+          <ListItem onClick={() => addIssuer()}>
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar>
+                  <AddIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary="Add" />
+            </ListItemButton>
           </ListItem>
-        ))}
-        <ListItem onClick={() => handleListItemClick()}>
-          <ListItemAvatar>
-            <Avatar>
-              <AddIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText primary="Add new issuer" />
-        </ListItem>
-      </List>
-    </Dialog>
+        </List>
+      </Dialog>
+        <Dialog open={addIssuerOpen} onClose={handleAddIssuerClose}>
+        <DialogTitle>Add Issuer</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter the URL of the issuer you would like to add without http/https(e.g. localhost:8090)
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="issuerURL"
+            label="Issuer URL"
+            type="url"
+            fullWidth
+            variant="standard"
+            onChange={handleUrlChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleAddIssuerClose}>Cancel</Button>
+          <Button onClick={() => addIssuerURL()}>Add</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
